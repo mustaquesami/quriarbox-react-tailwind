83c80d2213eaadd473e0c75f2cb22432ba15c090
mustaquesami <62767957+mustaquesami@users.noreply.github.com>
2023-11-28 05:33:33 +0600

    Testimonial section completed

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -4,9 +4,18 @@ import Img from './layouts/Img'
 import quote from '../assets/quote.png'
 import Container from './layouts/Container'
 import Flex from './layouts/Flex'
-import { IoMdStar } from "react-icons/io";
-import t1 from '../assets/t1.png'
 import Slider from "react-slick";
+import SliderContent from './layouts/SliderContent'
+import t1 from '../assets/t1.png'
+import { IoMdStar } from "react-icons/io";
+
+const generateStarIcons = (count) => {
+    const stars = [];
+    for (let i = 0; i < count; i++) {
+        stars.push(<IoMdStar key={i} className='text-[#F95C19]' />);
+    }
+    return stars;
+};
 
 const Testimonial = () => {
     const settings = {
@@ -27,50 +36,9 @@ const Testimonial = () => {
                 <Heading title='TESTIMONIAL' subTitle='Our Awesome Clients'/>
             </Flex>
             <Slider {...settings}>
-                <div>
-                    <div className='bg-white w-[872px] m-auto mt-[89px] rounded-2xl shadow-[rgba(0, 19, 119, 0.05)] px-10 pt-[40px] pb-[34px]'>
-                        <h3 className='w-[221px] text-[#F95C19] font-raleway text-2xl font-bold'>Fantastic service!</h3>
-                        <p className='w-[800px] text-[#464558] font-raleway text-base font-normal leading-6 mt-3 mb-2'>I purchased a phone from an e-commerce site, and this courier service provider assisted me in getting it delivered to my home. I received my phone within one day, and I was really satisfied with their service when I received it. They are really quick and dependable. They give me with the option of real-time delivery status, which allows me to track the progress of my goods delivery. I get a lot of questions from call center support and key account managers. They come highly recommended. Confidently say that they are really reliable.</p>
-                        <Flex className='items-center justify-between'>
-                            <Flex>
-                                <IoMdStar className='text-[#F95C19]' />
-                                <IoMdStar className='text-[#F95C19]' />
-                                <IoMdStar className='text-[#F95C19]' />
-                                <IoMdStar className='text-[#F95C19]' />
-                                <IoMdStar className='text-[#F95C19]' />
-                            </Flex>
-                            <Flex className='text-right font-raleway gap-x-3'>
-                                <div>
-                                    <h5 className='text-[#222132] font-bold text-base'>Yves Tanguy</h5>
-                                    <p className='text-[#464558] font-normal text-base'>Chief Executive, DLF</p>
-                                </div>
-                                <Img src={t1} className='w-[60px] h-[60px] rounded-full'/>
-                            </Flex>
-                        </Flex>
-                    </div>
-                </div>
-                <div>
-                    <div className='bg-white w-[872px] m-auto mt-[89px] rounded-2xl shadow-[rgba(0, 19, 119, 0.05)] px-10 pt-[40px] pb-[34px]'>
-                        <h3 className='w-[221px] text-[#F95C19] font-raleway text-2xl font-bold'>Fantastic service!</h3>
-                        <p className='w-[800px] text-[#464558] font-raleway text-base font-normal leading-6 mt-3 mb-2'>I purchased a phone from an e-commerce site, and this courier service provider assisted me in getting it delivered to my home. I received my phone within one day, and I was really satisfied with their service when I received it. They are really quick and dependable. They give me with the option of real-time delivery status, which allows me to track the progress of my goods delivery. I get a lot of questions from call center support and key account managers. They come highly recommended. Confidently say that they are really reliable.</p>
-                        <Flex className='items-center justify-between'>
-                            <Flex>
-                                <IoMdStar className='text-[#F95C19]' />
-                                <IoMdStar className='text-[#F95C19]' />
-                                <IoMdStar className='text-[#F95C19]' />
-                                <IoMdStar className='text-[#F95C19]' />
-                                <IoMdStar className='text-[#F95C19]' />
-                            </Flex>
-                            <Flex className='text-right font-raleway gap-x-3'>
-                                <div>
-                                    <h5 className='text-[#222132] font-bold text-base'>Yves Tanguy</h5>
-                                    <p className='text-[#464558] font-normal text-base'>Chief Executive, DLF</p>
-                                </div>
-                                <Img src={t1} className='w-[60px] h-[60px] rounded-full'/>
-                            </Flex>
-                        </Flex>
-                    </div>
-                </div>
+                <SliderContent title='Fantastic service!' desc='I purchased a phone from an e-commerce site, and this courier service provider assisted me in getting it delivered to my home. I received my phone within one day, and I was really satisfied with their service when I received it. They are really quick and dependable. They give me with the option of real-time delivery status, which allows me to track the progress of my goods delivery. I get a lot of questions from call center support and key account managers. They come highly recommended. Confidently say that they are really reliable.' name='Yves Tanguy' designation='Chief Executive, DLF' src={t1}>
+                    {generateStarIcons(5)}
+                </SliderContent>
             </Slider>
         </Container>
     </section>
---
+++ src/components/layouts/SliderContent.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Flex from './Flex'
+import Img from '../layouts/Img'
+
+const SliderContent = ({title,desc,children,name,designation,src}) => {
+  return (
+    <div>
+        <div className='bg-white w-[872px] m-auto mt-[89px] rounded-2xl shadow-[rgba(0, 19, 119, 0.05)] px-10 pt-[40px] pb-[34px]'>
+            <h3 className='w-[221px] text-[#F95C19] font-raleway text-2xl font-bold'>{title}</h3>
+            <p className='w-[800px] text-[#464558] font-raleway text-base font-normal leading-6 mt-3 mb-2'>{desc}</p>
+            <Flex className='items-center justify-between'>
+                <Flex>
+                  {children}
+                </Flex>
+                <Flex className='text-right font-raleway gap-x-3'>
+                    <div>
+                        <h5 className='text-[#222132] font-bold text-base'>{name}</h5>
+                        <p className='text-[#464558] font-normal text-base'>{designation}</p>
+                    </div>
+                    <Img src={src} className='w-[60px] h-[60px] rounded-full'/>
+                </Flex>
+            </Flex>
+        </div>
+      </div>
+  )
+}
+
+export default SliderContent
